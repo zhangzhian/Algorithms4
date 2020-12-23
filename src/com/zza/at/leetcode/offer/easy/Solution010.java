4de4dde58eaf4fdd7c5816091fbb43536052532b
zhangzhian <zhangzhian_123@qq.com>
2020-12-23 18:48:03 +0800

    Update Solution010.java

--- src/com/zza/at/leetcode/offer/easy/Solution010.java
+++ src/com/zza/at/leetcode/offer/easy/Solution010.java
@@ -33,6 +33,9 @@ public class Solution010 {
             }
             prev = prev.next;
         }
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 == null ? l2 : l1;
+
         return prehead.next;
     }
 }
