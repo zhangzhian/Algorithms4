a18ce1bfbba6a2b2be25c4379c7148b138d04769
zhangzhian <zhangzhian_123@qq.com>
2020-12-25 11:19:41 +0800

    Create Solution016.java

---
+++ src/com/zza/at/leetcode/hot100/mid/Solution016.java
@@ -0,0 +1,41 @@
+package com.zza.at.leetcode.hot100.mid;
+
+public class Solution016 {
+    public int findDuplicate(int[] nums) {
+        int temp;
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i]-1 != i){
+                //索引 与 索引对应的值 相等， 该数字已经存在了
+                if(nums[i] == nums[nums[i] -1]){
+                    return nums[i];
+                }
+                temp=nums[i];
+                nums[i]=nums[temp -1];
+                nums[temp-1]=temp;
+            }
+        }
+        return -1;
+    }
+
+    public int findDuplicate1(int[] nums) {
+        int slow = 0, fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        //[2,5,9,6,9,3,8,9,7,1]
+        //[1,3,4,2,2]
+        //[3,1,3,4,2]
+        int ret = new Solution016().findDuplicate1(new int[]{2,5,9,6,9,3,8,9,7,1});
+        System.out.println(ret);
+    }
+}
