ede402acb1aeaa0f1af70872a1df2a44288ea685
zhangzhian <zhangzhian_123@qq.com>
2020-12-25 11:19:36 +0800

    Update ListNode.java

--- src/com/zza/at/leetcode/common/ListNode.java
+++ src/com/zza/at/leetcode/common/ListNode.java
@@ -4,4 +4,5 @@ public class ListNode {
     public int val;
     public ListNode next;
     public ListNode(int x) { val = x; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
