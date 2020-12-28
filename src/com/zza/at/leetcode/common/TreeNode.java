bb41f7cc75cfb190fd9031d1b34cbb5ad804cfa1
zhangzhian <zhangzhian_123@qq.com>
2020-12-28 10:13:44 +0800

    hot100,mid,019-020

--- src/com/zza/at/leetcode/common/TreeNode.java
+++ src/com/zza/at/leetcode/common/TreeNode.java
@@ -4,6 +4,11 @@ public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
+    public TreeNode() {}
     public TreeNode(int x) { val = x; }
-
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+     }
 }
---
+++ src/com/zza/at/leetcode/hot100/mid/Solution020.java
@@ -0,0 +1,53 @@
+package com.zza.at.leetcode.hot100.mid;
+
+import com.zza.at.leetcode.common.TreeNode;
+
+//把二叉搜索树转换为累加树
+public class Solution020 {
+    int sum = 0;
+    //反序中序遍历
+    public TreeNode convertBST(TreeNode root) {
+        if (root != null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+
+    //Morris 遍历
+    public TreeNode convertBST1(TreeNode root) {
+        int sum = 0;
+        TreeNode node = root;
+
+        while (node != null) {
+            if (node.right == null) {
+                sum += node.val;
+                node.val = sum;
+                node = node.left;
+            } else {
+                TreeNode succ = getSuccessor(node);
+                if (succ.left == null) {
+                    succ.left = node;
+                    node = node.right;
+                } else {
+                    succ.left = null;
+                    sum += node.val;
+                    node.val = sum;
+                    node = node.left;
+                }
+            }
+        }
+
+        return root;
+    }
+
+    public TreeNode getSuccessor(TreeNode node) {
+        TreeNode succ = node.right;
+        while (succ.left != null && succ.left != node) {
+            succ = succ.left;
+        }
+        return succ;
+    }
+}
